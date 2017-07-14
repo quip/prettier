9ba011ec9a817864a75e9ba30bcf250656e2909e
Yang Su <yang@quip.com>
2017-07-14 11:39:41 -0700

    don't print extra space for self closing jsx elements

--- src/language-js/print/jsx.js
+++ src/language-js/print/jsx.js
@@ -541,8 +541,9 @@ function printJsxOpeningElement(path, options, print) {
     (node.typeParameters && hasComment(node.typeParameters));
 
   // Don't break self-closing elements with no attributes and no comments
+  const space = options.jsxBracketSameLine ? "" : " ";
   if (node.selfClosing && node.attributes.length === 0 && !nameHasComments) {
-    return ["<", print("name"), print("typeParameters"), " />"];
+    return ["<", print("name"), print("typeParameters"), space + "/>"];
   }
 
   // don't break up opening elements with a single long text attribute
@@ -570,7 +571,7 @@ function printJsxOpeningElement(path, options, print) {
       print("typeParameters"),
       " ",
       ...path.map(print, "attributes"),
-      node.selfClosing ? " />" : ">",
+      node.selfClosing ? space + "/>" : ">",
     ]);
   }
 
@@ -617,8 +618,8 @@ function printJsxOpeningElement(path, options, print) {
       print("name"),
       print("typeParameters"),
       indent(path.map(() => [attributeLine, print()], "attributes")),
-      node.selfClosing ? line : bracketSameLine ? ">" : softline,
-      node.selfClosing ? "/>" : bracketSameLine ? "" : ">",
+      bracketSameLine ? (node.selfClosing ? "/>" : ">") : softline,
+      bracketSameLine ? "" : node.selfClosing ? "/>" : ">",
     ],
     { shouldBreak }
   );
