bc2b31561969a9b40961f0ddbf1c96a6eebbef1c
Yang Su <yang@quip.com>
2017-07-14 11:39:41 -0700

    don't print extra space for self closing jsx elements

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -1912,12 +1912,13 @@ function printPathNoParens(path, options, print, args) {
         n.name && n.name.comments && n.name.comments.length > 0;
 
       // Don't break self-closing elements with no attributes and no comments
+      const space = options.jsxBracketSameLine ? "" : " ";
       if (n.selfClosing && !n.attributes.length && !nameHasComments) {
         return concat([
           "<",
           path.call(print, "name"),
           path.call(print, "typeParameters"),
-          " />"
+          space + "/>"
         ]);
       }
 
@@ -1947,7 +1948,7 @@ function printPathNoParens(path, options, print, args) {
             path.call(print, "typeParameters"),
             " ",
             concat(path.map(print, "attributes")),
-            n.selfClosing ? " />" : ">"
+            n.selfClosing ? space + "/>" : ">"
           ])
         );
       }
@@ -1989,9 +1990,9 @@ function printPathNoParens(path, options, print, args) {
                 path.map(attr => concat([line, print(attr)]), "attributes")
               )
             ),
-            n.selfClosing ? line : bracketSameLine ? ">" : softline
+            bracketSameLine ? (n.selfClosing ? "/>" : ">") : softline
           ]),
-          n.selfClosing ? "/>" : bracketSameLine ? "" : ">"
+          bracketSameLine ? "" : n.selfClosing ? "/>" : ">"
         ]),
         { shouldBreak }
       );
