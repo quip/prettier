df584cf4ecc5a959695350193618cc8a6adf7d70
Yang Su <yang@quip.com>
2017-07-28 13:15:19 -0700

    added parens-same-line option

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -1689,14 +1689,14 @@ function printPathNoParens(path, options, print, args) {
       );
     case "IfStatement": {
       const con = adjustClause(n.consequent, path.call(print, "consequent"));
+      const test = path.call(print, "test");
       const opening = group(
         concat([
           "if (",
           group(
-            concat([
-              indent(concat([softline, path.call(print, "test")])),
-              softline
-            ])
+            options.parensSameLine
+              ? indent(test)
+              : concat([indent(concat([softline, test])), softline])
           ),
           ")",
           con
@@ -3877,7 +3877,7 @@ function printArgumentsList(path, options, print) {
         "(",
         indent(concat([line, concat(printedArguments)])),
         maybeTrailingComma,
-        line,
+        options.parensSameLine ? "" : line,
         ")"
       ]),
       { shouldBreak: true }
@@ -3968,7 +3968,7 @@ function printArgumentsList(path, options, print) {
       "(",
       indent(concat([softline, concat(printedArguments)])),
       ifBreak(shouldPrintComma(options, "all") ? "," : ""),
-      softline,
+      options.parensSameLine ? "" : softline,
       ")"
     ]),
     { shouldBreak: printedArguments.some(willBreak) || anyArgEmptyLine }
@@ -4127,7 +4127,7 @@ function printFunctionParams(path, print, options, expandArg, printTypeParams) {
     ifBreak(
       canHaveTrailingComma && shouldPrintComma(options, "all") ? "," : ""
     ),
-    softline,
+    options.parensSameLine ? "" : softline,
     ")"
   ]);
 }
