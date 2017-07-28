57ded6f1ca73b9a544a562398ef3a270cf3013c2
Yang Su <yang@quip.com>
2017-07-28 13:15:19 -0700

    added parens-same-line option that put close parenthesis on the same line for if and call expressions, but not arguments in function declarations.

--- src/language-js/options.js
+++ src/language-js/options.js
@@ -98,4 +98,10 @@ module.exports = {
       },
     ],
   },
+  parensSameLine: {
+    type: "boolean",
+    category: CATEGORY_JAVASCRIPT,
+    default: false,
+    description: "Put ) on the last line instead of at a new line.",
+  },
 };
