33a913e0921525de0f9d19f6008bc663751da701
Yang Su <yang@quip.com>
2018-04-23 14:24:12 -0700

    added test:quip command
    - added example with a record type cast jsdoc comment
    - use quoteProps

--- tests/quip/jsfmt.spec.js
+++ tests/quip/jsfmt.spec.js
@@ -1,2 +1,6 @@
-run_spec(__dirname, { tabWidth: 4 });
-run_spec(__dirname, { tabWidth: 4, parensSameLine: true });
+run_spec(__dirname, ["flow", "typescript"], {
+  tabWidth: 4,
+  jsxBracketSameLine: true,
+  quoteProps: "preserve",
+  parensSameLine: true
+});
