fccf610ef4a152ad09c130b51d3644d3e148f8c9
Grace Kim <gracekim@slack-corp.com>
2025-09-02 15:02:50 -0700

    Quip fork of version 2.8.8

--- src/language-js/print/jsx.js
+++ src/language-js/print/jsx.js
@@ -545,7 +545,7 @@ function printJsxOpeningElement(path, options, print) {
     (node.typeParameters && hasComment(node.typeParameters));
 
   // Don't break self-closing elements with no attributes and no comments
-  const space = options.jsxBracketSameLine ? " " : "";
+  const space = options.bracketSameLine ? "" : "";
   if (node.selfClosing && node.attributes.length === 0 && !nameHasComments) {
     return ["<", print("name"), print("typeParameters"), space + "/>"];
   }
@@ -608,14 +608,17 @@ function printJsxOpeningElement(path, options, print) {
 }
 
 function printEndOfOpeningTag(node, options, nameHasComments) {
-  if (node.selfClosing) {
-    return [line, "/>"];
-  }
   const bracketSameLine = shouldPrintBracketSameLine(
     node,
     options,
     nameHasComments
   );
+  if (bracketSameLine) {
+    return node.selfClosing ? ["/>"] : [">"];
+  }
+  if (node.selfClosing) {
+    return [line, "/>"];
+  }
   if (bracketSameLine) {
     return [">"];
   }
